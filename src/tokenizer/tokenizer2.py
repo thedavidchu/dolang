992e8cf716f9c404086888080e139ab653c217b4
David Chu <thedavidchu@gmail.com>
2022-05-24 21:52:59 -0400

    Weak tokenizer ready

--- src/tokenizer/tokenizer2.py
+++ src/tokenizer/tokenizer2.py
@@ -38,7 +38,9 @@ is an error.
 
 import argparse
 from enum import Enum
-from typing import List, Tuple
+from lib2to3.pgen2 import token
+from multiprocessing.sharedctypes import Value
+from typing import List
 from typing_extensions import Self
 
 
@@ -49,7 +51,7 @@ class TokenType(str, Enum):
     WHITESPACE = "WHITESPACE"  # Indent vs non-indent
     COMMENT = "COMMENT"  # Multi-line vs non-multiline, nesting
     STRING = "STRING"  # Multi-quotes, single-vs-double quote
-    LITERAL = "LITERAL"  # Keyword, non-keyword
+    IDENTIFIER = "LITERAL"  # Keyword, non-keyword
     NUMBER = "NUMBER"  # Float, int, complex
     PUNCTUATION = "PUNCTUATION"  # Stand-alone, non-standalone
 
@@ -69,39 +71,6 @@ class Position:
 
     def new_col(self) -> Self:  # Assuming "Self" refers to its own type
         return Position(idx=self._idx + 1, line=self._line, col=self._col + 1)
-
-
-class Text:
-    def __init__(self, text: str):
-        self.text: str = text
-        self.position: Position = Position(0, 1, 0)
-
-    def __next__(self) -> str:
-        if self.position._idx + 1 >= len(self.text):
-            return EOF
-
-        c: str = self.text[self.position._idx]
-        if c == "\n":
-            self.position = self.position.new_line()
-        else:
-            self.position = self.position.new_col()
-        return self.text[self.position._idx]
-
-    def getoffset(self, idx: int) -> str:
-        if len(self.text) > self.position._idx + idx:
-            return self.text[self.position._idx + idx]
-        else:
-            return EOF
-
-    def getpos(self) -> Position:
-        return self.position
-
-    def fastforward(self, num: int) -> None:
-        for i in range(num):
-            next(self)
-
-    def cutouttoken(self, offset: int) -> str:
-        return self.text[self.position._idx : self.position._idx + offset]
 
 
 class Token:
@@ -125,6 +94,64 @@ class Token:
         return f"{start_line}:{start_col}-{end_line}:{end_col}:\t{self.token_type}\t{sanitized_strtoken}"
 
 
+class Text:
+    def __init__(self, text: str):
+        self.text: str = text
+        self.position: Position = Position(0, 1, 0)
+
+    def viewnext(self, offset: int) -> str:
+        if offset < 0:
+            raise ValueError("idx must be non-zero!")
+        if self.position._idx + offset >= len(self.text):
+            return EOF
+        return self.text[self.position._idx + offset]
+
+    def __next__(self) -> str:
+        if self.position._idx + 1 > len(self.text):
+            return EOF
+
+        c: str = self.viewnext(0)
+        if c == "\n":
+            self.position = self.position.new_line()
+        else:
+            self.position = self.position.new_col()
+        return self.viewnext(0)
+
+    def is_eof(self) -> bool:
+        return self.viewnext(0) == EOF
+
+    def viewnext(self, offset: int) -> str:
+        if offset < 0:
+            raise ValueError("idx must be non-zero!")
+        if self.position._idx + offset >= len(self.text):
+            return EOF
+        return self.text[self.position._idx + offset]
+
+    def getpos(self) -> Position:
+        return self.position
+
+    def fastforward(self, num: int) -> None:
+        for _ in range(num):
+            next(self)
+
+    def capturetoken(self, offset: int, token_type: TokenType) -> Token:
+        if offset < 0:
+            raise ValueError("offset must be non-zero!")
+        if self.position._idx + offset > len(self.text):
+            return None
+        strtoken = self.text[self.position._idx : self.position._idx + offset]
+        startpos: Position = self.getpos()
+        self.fastforward(offset)
+        endpos: Position = self.getpos()
+        token = Token(
+            strtoken=strtoken,
+            ttype=token_type,
+            startpos=startpos,
+            endpos=endpos,
+        )
+        return token
+
+
 """
 Steps
 -----
@@ -142,25 +169,25 @@ def is_whitespace_start(text: Text) -> bool:  # [ \n\t\v\r]+
     """Note: the word "_start" in the function name is unnecessary. It is the
     start, the middle, and the end, because all characters fall into the same
     group."""
-    c = text.getoffset(0)
+    c = text.viewnext(0)
     assert c != EOF
     return c.isspace()
 
 
 def is_comment_start(text: Text) -> bool:
-    c = text.getoffset(0)
-    next_c = text.getoffset(1)
+    c = text.viewnext(0)
     assert c != EOF
     if c == "#":
         return True
-    elif c == "/" and (next_c == "*" or next_c == "/"):
+    next_c = text.viewnext(1)
+    if c == "/" and (next_c == "*" or next_c == "/"):
         return True
     return False
 
 
 def is_string_start(text: Text) -> bool:
     """We will only support strings surrounded by single double quotes for now."""
-    c = text.getoffset(0)
+    c = text.viewnext(0)
     assert c != EOF
     return c == '"'
 
@@ -168,19 +195,22 @@ def is_string_start(text: Text) -> bool:
 def is_number_start(text: Text) -> bool:
     """We will not support 0. or .0 numbers for now. + 10 is also not a single
     number."""
-    c = text.getoffset(0)
-    next_c = text.getoffset(1)
+    c = text.viewnext(0)
     assert c != EOF
     if c.isdecimal():
         return True
     elif c == "+" or c == "-":
-        # Assuming next_c is not EOF... not that it would matter.
+        next_c = text.viewnext(1)
+        # We test if next_c is EOF, because a string method will not work on a
+        # non-string type.
+        if next_c == EOF:
+            return False
         return next_c.isdecimal()  # [0-9]
     return False
 
 
-def is_literal_start(text: Text) -> bool:  # [A-Za-z_][A-Za-z0-9_]*
-    c = text.getoffset(0)
+def is_identifier_start(text: Text) -> bool:  # [A-Za-z_][A-Za-z0-9_]*
+    c = text.viewnext(0)
     assert c != EOF
     return c.isalpha() or c == "_"
 
@@ -188,14 +218,14 @@ def is_literal_start(text: Text) -> bool:  # [A-Za-z_][A-Za-z0-9_]*
 def is_standalone_punctuation_start(text: Text) -> bool:  # [()[]{},;]
     """Note: the word "_start" in the function name is unnecessary. It is the
     start, the middle, and the end, because this is only one character."""
-    c = text.getoffset(0)
+    c = text.viewnext(0)
     assert c != EOF
     return c in {"(", ")", "[", "]", "{", "}", ";", ","}
 
 
 def is_groupable_punctuation_start(text: Text) -> bool:
     """The punctuation characters _can_ be grouped, e.g. +=, **=, etc."""
-    c = text.getoffset(0)
+    c = text.viewnext(0)
     assert c != EOF
     if c in "~!@$%^&*-+=|:<>./?":
         return True
@@ -206,26 +236,15 @@ def is_groupable_punctuation_start(text: Text) -> bool:
 ########## EXTRACTION FUNCTIONS ################################################
 ################################################################################
 def capture_whitespace(text: Text) -> "Token | None":
-    c = text.getoffset(0)
+    c = text.viewnext(0)
     assert c != EOF
 
-    startpos = text.getpos()
     i = 0
-    while c.isspace():
+    while c != EOF and c.isspace():
         i += 1
-        c = text.getoffset(i)
+        c = text.viewnext(i)
 
-    strtoken = text.cutouttoken(i)
-    text.fastforward(i)
-    endpos = text.getpos()
-
-    token = Token(
-        strtoken=strtoken,
-        ttype=TokenType.WHITESPACE,
-        startpos=startpos,
-        endpos=endpos,
-    )
-    return token
+    return text.capturetoken(i, TokenType.WHITESPACE)
 
 
 def capture_comment(text: Text) -> "Token | None":
@@ -234,11 +253,10 @@ def capture_comment(text: Text) -> "Token | None":
     // This ends in \n or EOF.
     /* This can next but cannot end without its ending, which is */.
     """
-    c = text.getoffset(0)
-    next_c = text.getoffset(1)
+    c = text.viewnext(0)
+    next_c = text.viewnext(1)
     assert c != EOF
 
-    startpos = text.getpos()
     if c == "#":
         start_comment = "#"
         next_idx = 1
@@ -252,93 +270,161 @@ def capture_comment(text: Text) -> "Token | None":
         raise ValueError("unexpected comment starter")
 
     while True:
-        next_c = text.getoffset(next_idx)
+        next_c = text.viewnext(next_idx)
 
-        if next_c == EOF:
-            if start_comment == "#" or start_comment == "//":
-                strtoken = text.cutouttoken(next_idx)
-                text.fastforward(next_idx)
-                endpos = text.getpos()
-                token = Token(
-                    strtoken=strtoken,
-                    ttype=TokenType.COMMENT,
-                    startpos=startpos,
-                    endpos=endpos,
-                )
+        if start_comment == "#":
+            print(f"> {next_c=}")
+            if next_c == "\n":
+                token = text.capturetoken(next_idx + 1, TokenType.COMMENT)
                 return token
-            raise ValueError("unexpected EOF!")
-        # End-of-line
-        if next_c == "\n" and (start_comment == "#" or start_comment == "//"):
-            strtoken = text.cutouttoken(next_idx)
-            text.fastforward(next_idx)
-            endpos = text.getpos()
-            token = Token(
-                strtoken=strtoken,
-                ttype=TokenType.COMMENT,
-                startpos=startpos,
-                endpos=endpos,
-            )
-            return token
-        # Deal with: */
-        if next_c == "*" and start_comment == "/*":
-            next_next_c = text.getoffset(next_idx + 1)
-            if next_next_c != "/":
-                # Must do at the end
-                next_idx += 1
-                continue
-            next_idx += 1
-            strtoken = text.cutouttoken(next_idx)
-            text.fastforward(next_idx)
-            endpos = text.getpos()
-            token = Token(
-                strtoken=strtoken,
-                ttype=TokenType.COMMENT,
-                startpos=startpos,
-                endpos=endpos,
-            )
-            return token
+            elif next_c == EOF:
+                token = text.capturetoken(next_idx, TokenType.COMMENT)
+                return token
+            # increment next_idx, continue
+        elif start_comment == "//":
+            if next_c == "\n":
+                token = text.capturetoken(next_idx + 1, TokenType.COMMENT)
+                return token
+            elif next_c == EOF:
+                token = text.capturetoken(next_idx, TokenType.COMMENT)
+                return token
+            # increment next_idx, continue
+        elif start_comment == "/*":
+            if next_c == EOF:
+                raise ValueError("unexpected EOF!")
+            if next_c == "*":
+                next_next_c = text.viewnext(next_idx + 1)
+                if next_next_c == "/":
+                    token = text.capturetoken(next_idx + 2, TokenType.COMMENT)
+                    return token
+        else:
+            raise ValueError("unexpected comment started")
 
         # Must do at the end
         next_idx += 1
+    return None
 
 
 def capture_string(text: Text) -> "Token | None":
-    c = text.getoffset(0)
-    next_c = text.getoffset(1)
+    c = text.viewnext(0)
     assert c != EOF
 
-    startpos = text.getpos()
     if c != '"':
         raise ValueError("unexpected starting character")
 
     next_idx = 1
+    next_c = text.viewnext(1)
     while True:
-        next_c = text.getoffset(next_idx)
-
-        # Skip \"
+        next_c = text.viewnext(next_idx)
+        # Skip '"'
         if next_c == "\\":
-            next_idx += 1
-            next_c = text.getoffset(next_idx)
-            if next_c not in {"'", '"', "\\", "n", "r", "t", "b", "f", "v"}:
+            next_next_c = text.viewnext(next_idx + 1)
+            if next_next_c not in {
+                "'",
+                '"',
+                "\\",
+                "n",
+                "r",
+                "t",
+                "b",
+                "f",
+                "v",
+            }:
                 raise ValueError(f"unexpected escaped character {next_c}")
-            next_idx += 1
+            next_idx += 2
             continue
         if next_c == '"':
-            strtoken = text.cutouttoken(next_idx + 1)
-            text.fastforward(next_idx + 1)
-            endpos = text.getpos()
-            token = Token(
-                strtoken=strtoken,
-                ttype=TokenType.STRING,
-                startpos=startpos,
-                endpos=endpos,
-            )
+            print('\t> "')
+            token = text.capturetoken(next_idx + 1, TokenType.STRING)
+            print(f"\t> {text.viewnext(0)}")
             return token
         if next_c == EOF:
             raise ValueError("end of string without finishing quote!")
 
         # Call at the end
         next_idx += 1
+
+
+def capture_number(text: Text) -> "Token | None":
+    c = text.viewnext(0)
+    assert c != EOF
+
+    next_idx = 0
+    if c == "+" or c == "-":
+        if not text.viewnext(1).isdecimal():
+            # We call an error instead of passing this off because we checked
+            # for it in is_number_start()
+            raise ValueError("expected number!")
+        next_idx = 1
+    elif not c.isdecimal():
+        raise ValueError("expected number!")
+
+    while True:
+        next_c = text.viewnext(next_idx)
+        if next_c not in "1234567890.":
+            return text.capturetoken(next_idx, TokenType.NUMBER)
+        next_next_c = text.viewnext(next_idx + 1)
+        # Separate ..
+        if next_c == "." and next_next_c == ".":
+            return text.capturetoken(next_idx, TokenType.NUMBER)
+        next_idx += 1
+
+
+def capture_identifier(text: Text) -> "Token | None":
+    c = text.viewnext(0)
+    assert c != EOF
+
+    if not c.isalpha() and c != "_":
+        raise ValueError("Unexpected literal start character!")
+
+    idx = 1
+    while True:
+        c = text.viewnext(idx)
+        if not c.isalnum() and c != "_":
+            return text.capturetoken(idx, TokenType.IDENTIFIER)
+        idx += 1
+
+
+def capture_standalone_punctuation(text: Text) -> "Token | None":
+    c = text.viewnext(0)
+    assert c != EOF
+
+    if not c in r"(){}[],;":
+        raise ValueError("unexpected character!")
+    return text.capturetoken(1, TokenType.PUNCTUATION)
+
+
+def capture_groupable_punctuation(text: Text) -> "Token | None":
+    """
+    Single Characters: ~ ! @ % ^ & * - + = | : < > . ?
+    Double Characters: ~= != @= %= ^= &= *= -= += == |= := <= >= ?= ** && || ..
+    Triple Characters: === **= ... ..=
+    """
+    c = text.viewnext(0)
+    assert c != EOF
+
+    if not c in "~!@$%^&*-+=|:<>./?":
+        raise ValueError("unexpected character!")
+
+    first_c = text.viewnext(0)
+    second_c = text.viewnext(1)
+    third_c = text.viewnext(2)
+
+    three_char = "=== **= ... ..=".split(" ")
+    two_char = "~= != @= %= ^= &= *= -= += == |= := <= >= ?= ** && || ..".split(
+        " "
+    )
+    one_char = "~ ! @ % ^ & * - + = | : < > . ?".split(" ")
+
+    # Select greedily
+    if (first_c, second_c, third_c) in set(map(tuple, three_char)):
+        return text.capturetoken(3, TokenType.PUNCTUATION)
+    elif (first_c, second_c) in set(map(tuple, two_char)):
+        return text.capturetoken(2, TokenType.PUNCTUATION)
+    elif (first_c,) in set(map(tuple, one_char)):
+        return text.capturetoken(1, TokenType.PUNCTUATION)
+
+    raise ValueError("unknown groupable type!")
 
 
 ################################################################################
@@ -357,14 +443,12 @@ def get_text() -> Text:
 
 def get_tokens(text: Text) -> List[Token]:
     tokens = []
-    c = text.getoffset(0)
-    while c != EOF:
+    while not text.is_eof():
+        print(f"{len(tokens)=}, {text.viewnext(0)=}")
         if is_whitespace_start(text):
             t = capture_whitespace(text)
             if t is not None:
                 tokens.append(t)
-                # Run this after every iteration
-                c = next(text)
                 continue
             # Otherwise, failure to capture whitespace
             raise ValueError(
@@ -374,8 +458,6 @@ def get_tokens(text: Text) -> List[Token]:
             t = capture_comment(text)
             if t is not None:
                 tokens.append(t)
-                # Run this after every iteration
-                c = next(text)
                 continue
             # Otherwise, failure to capture comment
             raise ValueError(
@@ -385,17 +467,48 @@ def get_tokens(text: Text) -> List[Token]:
             t = capture_string(text)
             if t is not None:
                 tokens.append(t)
-                # Run this after every iteration
-                c = next(text)
                 continue
             # Otherwise, failure to capture comment
             raise ValueError(
                 "failed to capture string, even though I'm sure it was a string!"
             )
-
-        # Run this after every iteration
-        c = next(text)
-        continue
+        if is_number_start(text):
+            t = capture_number(text)
+            if t is not None:
+                tokens.append(t)
+                continue
+            # Otherwise, failure to capture comment
+            raise ValueError(
+                "failed to capture number, even though I'm sure it was a number!"
+            )
+        if is_identifier_start(text):
+            t = capture_identifier(text)
+            if t is not None:
+                tokens.append(t)
+                continue
+            # Otherwise, failure to capture comment
+            raise ValueError(
+                "failed to capture identifier, even though I'm sure it was a identifier!"
+            )
+        if is_standalone_punctuation_start(text):
+            t = capture_standalone_punctuation(text)
+            if t is not None:
+                tokens.append(t)
+                continue
+            # Otherwise, failure to capture comment
+            raise ValueError(
+                "failed to capture stand-alone punctuation, even though I'm sure it was a stand-alone punctuation!"
+            )
+        if is_groupable_punctuation_start(text):
+            t = capture_groupable_punctuation(text)
+            if t is not None:
+                tokens.append(t)
+                continue
+            # Otherwise, failure to capture comment
+            raise ValueError(
+                "failed to capture groupable punctuation, even though I'm sure it was a groupable punctuation!"
+            )
+        next(text)
     return tokens
 
 
