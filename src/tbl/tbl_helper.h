b7756e2264fbf538cc8e70af5fe16f9344d2a95a
David Chu <45278895+thedavidchu@users.noreply.github.com>
2022-06-07 16:51:15 -0400

    Update tbl_helper.h (#50)
    * Update tbl_helper.h
    
    * Fixed helper.

--- src/tbl/tbl_helper.h
+++ src/tbl/tbl_helper.h
@@ -42,24 +42,25 @@ static inline int item_idx_print(const size_t item_idx) {
     }
 }
 
-/** This has three different modes.
+/** Get the table index that the key has (or would have if it is not present).
+ *
+ * This has two different modes.
  * 1. Insert: search for the key as if it is already in the array. Stop when we
  * have searched the entire table. If it is not in the array, then return the
  * first TOMBSTONE (or error if no tombstone).
- * 2. Search: search for the key as if it is already in the array. Not finding
- * the key is not an error.
- * 3. Remove: search for the key as if it is already in the array. Not finding
- * the key is not an error.
- *
- * We can approximate this by
+ * 2. Search/Remove: search for the key as if it is already in the array. Not
+ * finding the key is not an error. We skip over tombstones and do not need to
+ * track them.
  */
 static int tbl_gettableidx(const tbl *const restrict me, const void *const key,
                            const bool return_tombstone,
                            size_t *const table_idx_p) {
-    size_t first_tombstone = INVALID;
-    size_t hashcode = 0, table_home = 0, table_offset = 0, table_idx = 0,
-           items_idx = 0;
 
+    size_t first_tombstone = INVALID, hashcode = 0, table_home = 0,
+           table_offset = 0;
+
+    /* Optimization: remove these and indicate that this function performs no
+    checks. */
     RETURN_IF_ERROR(me == NULL, ERROR_NULLPTR);
     RETURN_IF_ERROR(key == NULL, ERROR_NULLPTR);
     RETURN_IF_ERROR(table_idx_p == NULL, ERROR_NULLPTR);
@@ -69,14 +70,15 @@ static int tbl_gettableidx(const tbl *const restrict me, const void *const key,
     table_home = hashcode % me->cap;
 
     for (table_offset = 0; table_offset < me->cap; ++table_offset) {
-        table_idx = (table_home + table_offset) % me->cap;
-        items_idx = me->table[table_idx];
-        if (items_idx == INVALID) {
+        const size_t table_idx = (table_home + table_offset) % me->cap;
+        const size_t items_idx = me->table[table_idx];
+
+        if (items_idx == INVALID) { /* key not present */
             *table_idx_p = table_idx;
             return 0;
         } else if (items_idx == TOMBSTONE) {
-            /* Record first instance of tombstone in table. We will fall back
-            to this if necessary. */
+            /* Record first instance of tombstone in table. We will fall
+            back to this if necessary. */
             if (return_tombstone && first_tombstone == INVALID) {
                 first_tombstone = table_idx;
             }
@@ -84,24 +86,27 @@ static int tbl_gettableidx(const tbl *const restrict me, const void *const key,
             searching for. */
             continue;
         } else {
+            /* We use an if-else ladder because we wish to declare a variable
+            within the else-clause. */
             const tbl_kv *const item = arr_search(&me->items, items_idx);
+
             assert(item != NULL && "unexpected NULL item");
-            if (item->hashcode != hashcode ||
-                me->key_cmp(item->key, key) != 0) {
-                continue;
+            if (item->hashcode == hashcode &&
+                me->key_cmp(item->key, key) == 0) { /* key present */
+                *table_idx_p = table_idx;
+                return 0;
             }
-            *table_idx_p = table_idx;
-            return 0;
+            continue;
         }
-        assert(0 && "should not get here because all returns or continues!");
     }
 
-    /* Account for very weird corner case. If have no INVALID or matching items
-    but we have tombstones, we will have space to insert. */
     if (return_tombstone && first_tombstone == INVALID) {
         /* No space and not found */
         return ERROR_NOROOM;
-    } else if (return_tombstone) {
+    }
+    /* Account for very weird corner case. If have no INVALID or matching items
+    but we have tombstones, we will have space to insert. */
+    if (return_tombstone) {
         *table_idx_p = first_tombstone;
         return 0;
     } else {
