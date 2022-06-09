df537a79625a2eb30e79bc7e8c5f5b6e27306b9d
David Chu <thedavidchu@gmail.com>
2022-06-08 22:35:58 -0400

    Downloaded ThrowTheSwitch's Unity testing framework. Created first (non-working) test.

---
+++ src/unit-tests/test_mem.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "file_to_test.h"
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    return UNITY_END();
+}
